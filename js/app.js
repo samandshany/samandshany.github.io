799181abdb038dd87e1b7919277b8f99d13f8c5f
samandshany <samchrisp@Sams-MacBook-Air.local>
2014-02-16 22:30:19 +1100

    Added countdown.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+//$(".countdown").TimeCircles();
+
+$(".countdown").TimeCircles({
+		"circle_bg_color": "#000000",
+    "time": {
+        "Days": {
+            "text": "Days",
+            "color": "#ffff00"
+        },
+        "Hours": {
+            "text": "Hours",
+            "color": "#ffff00"
+        },
+        "Minutes": {
+            "text": "Minutes",
+            "color": "#ffff00"
+        },
+        "Seconds": {
+            "text": "Seconds",
+            "color": "#ffff00"
+        }
+    }
+});
